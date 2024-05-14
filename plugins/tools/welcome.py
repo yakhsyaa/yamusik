21a5a9e6743db74c40f0f071e8bd8e41fcf2e26c
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-14 21:52:24 +0530

    Update welcome.py

--- plugins/tools/welcome.py
+++ plugins/tools/welcome.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from DAXXMUSIC import app
+from AnonXMusic import app
 
 
 
