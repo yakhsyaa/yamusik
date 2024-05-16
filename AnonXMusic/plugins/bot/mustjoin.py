147d833bfec636aa77d5ab335ad9038dfd3f3f2e
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-16 09:05:04 +0530

    Update mustjoin.py

--- AnonXMusic/plugins/bot/mustjoin.py
+++ AnonXMusic/plugins/bot/mustjoin.py
@@ -1,3 +1,4 @@
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
