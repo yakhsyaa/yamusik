9e6a25a006aaad2f032cc22a644020bc5c9d71be
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-05 13:47:58 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -3,7 +3,7 @@ import time
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from youtubesearchpython.future import VideosSearch
+from youtubesearchpython._future_ import VideosSearch
 
 import config
 from AnonXMusic import app
