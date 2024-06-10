ab032eea78e2d18a7850cdf55f9fc60a252d9fd8
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-10 21:53:19 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -68,8 +68,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-           
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],       
     ]
     return buttons
 
