922ec747d7e6bc92a51f5c2b65cb1af2ad31b1f2
GDN Bharath <dn6418138@gmail.com>
2024-09-25 19:15:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(getenv("OWNER_ID", 6896043885))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/dattudd/Thanumusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/dattudd/Anonxmusic-")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
 
