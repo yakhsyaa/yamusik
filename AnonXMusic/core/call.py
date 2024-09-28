14b7e33d504da1f0f7f452199b3d4ad0a8dcc717
GDN Bharath <dn6418138@gmail.com>
2024-09-28 13:34:19 +0530

    Update call.py

--- AnonXMusic/core/call.py
+++ AnonXMusic/core/call.py
@@ -6,7 +6,7 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.exceptions import (
+from pytgcalls.exceptions import Anony
     AlreadyJoinedError,
     NoActiveGroupCall,
     TelegramServerError,
