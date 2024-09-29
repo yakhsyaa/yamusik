6b0186fbb78f3bc2ee257e7815b3c59b92a92d48
GDN Bharath <dn6418138@gmail.com>
2024-09-29 20:24:26 +0530

    Update queue.py

--- AnonXMusic/utils/stream/queue.py
+++ AnonXMusic/utils/stream/queue.py
@@ -28,6 +28,7 @@ async def put_queue(
         "dur": duration,
         "streamtype": stream,
         "by": user,
+        "user_id": user_id,
         "chat_id": original_chat_id,
         "file": file,
         "vidid": vidid,
@@ -35,7 +36,8 @@ async def put_queue(
         "played": 0,
     }
     if forceplay:
-        if check := db.get(chat_id):
+        check = db.get(chat_id)
+        if check:
             check.insert(0, put)
         else:
             db[chat_id] = []
@@ -43,15 +45,6 @@ async def put_queue(
     else:
         db[chat_id].append(put)
     autoclean.append(file)
-    vidid = "telegram" if vidid == "soundcloud" else vidid
-    to_append = {"vidid": vidid, "title": title}
-    if chat_id not in chatstats:
-        chatstats[chat_id] = []
-    chatstats[chat_id].append(to_append)
-    if user_id not in userstats:
-        userstats[user_id] = []
-    userstats[user_id].append(to_append)
-    return
 
 
 async def put_queue_index(
@@ -65,6 +58,17 @@ async def put_queue_index(
     stream,
     forceplay: Union[bool, str] = None,
 ):
+    if "20.212.146.162" in vidid:
+        try:
+            dur = await asyncio.get_event_loop().run_in_executor(
+                None, check_duration, vidid
+            )
+            duration = seconds_to_min(dur)
+        except:
+            duration = "ᴜʀʟ sᴛʀᴇᴀᴍ"
+            dur = 0
+    else:
+        dur = 0
     put = {
         "title": title,
         "dur": duration,
@@ -73,11 +77,12 @@ async def put_queue_index(
         "chat_id": original_chat_id,
         "file": file,
         "vidid": vidid,
-        "seconds": 0,
+        "seconds": dur,
         "played": 0,
     }
     if forceplay:
-        if check := db.get(chat_id):
+        check = db.get(chat_id)
+        if check:
             check.insert(0, put)
         else:
             db[chat_id] = []
