8f65e3b789657424462691deadb3f0fcc11bf36d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-05 14:43:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
+BOT_USERNAME = getenv("BOT_USERNAME")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
