497eababf728bb20a49ea35d646dc3d0bc3754b2
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-05 17:31:26 +0700

    Update bot.py

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -52,6 +52,3 @@ class Anony(Client):
 
     async def stop(self):
         await super().stop()
-pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
-dispatcher = updater.dispatcher
-aiohttpsession = ClientSession()
